d91ae17d546b20e7343954be9c2faa93a6f7dcd5
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-05-29 00:26:32 +0530

    feat: refactor MaintainerCard to use MaintainerImage component for improved image handling

--- components/MaintainerCard.vue
+++ components/MaintainerCard.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
-const colorMode = useColorMode();
 defineProps(["maintainer"]);
-
-const getMaintainerPlaceholderImage = () => {
-  if (colorMode.value === "dark") {
-    return "/maintainer_photo_dark.svg";
-  } else if (colorMode.value === "light") {
-    return "/maintainer_photo_light.svg";
-  }
-
-  return "/maintainer_photo_dark.svg";
-};
 </script>
 <template>
   <div
@@ -20,16 +9,7 @@ const getMaintainerPlaceholderImage = () => {
     <div
       class="p-8 flex gap-4 items-center bg-tertiary-light dark:bg-tertiary-dark"
     >
-      <img
-        class="w-8 h-8 aspect-square"
-        v-if="maintainer.photo"
-        :src="maintainer.photo"
-      />
-      <img
-        class="w-8 h-8 aspect-square"
-        v-else
-        :src="getMaintainerPlaceholderImage()"
-      />
+      <MaintainerImage :maintainer="maintainer" />
       <div class="flex flex-col">
         <h4 class="font-bold">{{ maintainer.full_name }}</h4>
         <p class="text-sm">
---
+++ components/MaintainerImage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+defineProps(["maintainer"]);
+const getMaintainerPlaceholderImage = () => {
+  if (colorMode.value === "dark") {
+    return "/maintainer_photo_dark.svg";
+  } else if (colorMode.value === "light") {
+    return "/maintainer_photo_light.svg";
+  }
+
+  return "/maintainer_photo_dark.svg";
+};
+</script>
+
+<template>
+  <img
+    class="w-12 h-12 aspect-square"
+    v-if="maintainer.photo"
+    :src="maintainer.photo"
+  />
+  <img
+    v-else
+    class="w-12 h-12 aspect-square"
+    :src="getMaintainerPlaceholderImage()"
+  />
+</template>
