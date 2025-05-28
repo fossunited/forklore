db57c01cf0ee23b6f8bf780e7ea4d1ec4ec674a5
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-05-28 21:08:16 +0530

    feat: update maintainer card to use direct properties and enhance content schema

--- components/MaintainerCard.vue
+++ components/MaintainerCard.vue
@@ -15,15 +15,15 @@ const getMaintainerPlaceholderImage = () => {
 <template>
   <div
     class="flex flex-col border outline-0 hover:outline-1 hover:cursor-pointer"
-    @click="$router.push(maintainer.meta.path)"
+    @click="$router.push(maintainer.path)"
   >
     <div
       class="p-8 flex gap-4 items-center bg-tertiary-light dark:bg-tertiary-dark"
     >
       <img
         class="w-8 h-8 aspect-square"
-        v-if="maintainer.meta.photo"
-        :src="maintainer.meta.photo"
+        v-if="maintainer.photo"
+        :src="maintainer.photo"
       />
       <img
         class="w-8 h-8 aspect-square"
@@ -40,13 +40,13 @@ const getMaintainerPlaceholderImage = () => {
     <div
       class="grid grid-cols-1"
       :class="
-        maintainer.meta.projects.length > 1
+        maintainer.projects.length > 1
           ? 'md:grid-cols-2 divide-x-custom'
           : 'md:grid-cols-1'
       "
     >
       <ProjectCard
-        v-for="project in maintainer.meta.projects"
+        v-for="project in maintainer.projects"
         :key="project.name"
         :project="project"
       />
--- content.config.ts
+++ content.config.ts
@@ -3,12 +3,24 @@ import { defineCollection, defineContentConfig, z } from "@nuxt/content";
 export default defineContentConfig({
   collections: {
     maintainers: defineCollection({
-      type: "data",
+      type: "page",
       source: "maintainers/**.json",
       schema: z.object({
         username: z.string(),
         full_name: z.string(),
+        photo: z.string(),
         designation: z.string(),
+        git_link: z.string(),
+        projects: z.array(
+          z.object({
+            name: z.string(),
+            project_link: z.string(),
+            website_link: z.string(),
+            logo: z.string(),
+            description: z.string(),
+            short_description: z.string(),
+          })
+        ),
       }),
     }),
   },
