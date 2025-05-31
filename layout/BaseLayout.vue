169774e6519cb0ab21c2df4b283da502b81ce93c
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-05-31 19:04:21 +0530

    fix: add padding to BaseLayout for improved responsiveness

--- layout/BaseLayout.vue
+++ layout/BaseLayout.vue
@@ -2,7 +2,9 @@
 import { Header } from "#components";
 </script>
 <template>
-  <div class="w-full place-items-center flex flex-col items-center">
+  <div
+    class="w-full place-items-center flex flex-col px-4 md:px-0 items-center"
+  >
     <div
       class="max-w-screen-md w-full min-h-svh border-custom !border-t-0 !border-b-0"
     >
