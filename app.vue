03ea74389d6b595f114f8b23e24a29a62f4283fc
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-05-31 19:25:52 +0530

    feat: add SEO metadata and Open Graph image for improved sharing

--- app.vue
+++ app.vue
@@ -8,6 +8,22 @@ useHead({
     { name: "viewport", content: "width=device-width, initial-scale=1" },
   ],
 });
+
+useSeoMeta({
+  title: "Forklore",
+  ogTitle: "Forklore",
+  description:
+    "Confessions, quirks and occasional rants from India’s open source keepers",
+  ogDescription:
+    "Confessions, quirks and occasional rants from India’s open source keepers",
+  ogImage: {
+    url: "/og_image_main.png",
+    width: 1200,
+    height: 630,
+    type: "image/png",
+  },
+  twitterCard: "summary_large_image",
+});
 </script>
 <template>
   <BaseLayout>
