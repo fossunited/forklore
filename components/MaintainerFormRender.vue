3034e2e152b31e73e74be7c593140d3063d01fb2
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-06-02 17:31:50 +0530

    fix: update MaintainerFormRender to conditionally display responses

--- components/MaintainerFormRender.vue
+++ components/MaintainerFormRender.vue
@@ -3,21 +3,22 @@ defineProps(["maintainer"]);
 </script>
 <template>
   <div class="flex flex-col p-8 gap-12">
-    <div
-      class="flex flex-col gap-6 text-base font-normal"
-      v-for="item in maintainer.form"
-      :key="item.question"
-    >
-      <div class="bg-secondary-light dark:bg-secondary-dark p-2">
-        <p class="text-primary-light dark:text-primary-dark">
-          {{ item.question }}
-        </p>
+    <template v-for="item in maintainer.form" :key="item.question">
+      <div
+        class="flex flex-col gap-6 text-base font-normal"
+        v-if="item.response"
+      >
+        <div class="bg-secondary-light dark:bg-secondary-dark p-2">
+          <p class="text-primary-light dark:text-primary-dark">
+            {{ item.question }}
+          </p>
+        </div>
+        <div>
+          <p class="text-secondary-light dark:text-secondary-dark">
+            {{ item.response }}
+          </p>
+        </div>
       </div>
-      <div>
-        <p class="text-secondary-light dark:text-secondary-dark">
-          {{ item.response }}
-        </p>
-      </div>
-    </div>
+    </template>
   </div>
 </template>
