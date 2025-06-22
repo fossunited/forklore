db45ac31e88cf24b59ef5f8dd7dd870a81c3dc5e
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-06-22 19:32:00 +0530

    feat: add search functionality in homepage

---
+++ components/ui/SearchInput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const query = defineModel<string>();
+</script>
+<template>
+  <div
+    class="flex items-center px-4 py-2 gap-4 border has-focus:outline-1 transition-all duration-300 ease-linear"
+  >
+    <IconsSearch class="w-4 h-4" />
+    <input
+      v-model="query"
+      type="text"
+      placeholder="Search"
+      class="input w-full text-sm focus:outline-none"
+    />
+  </div>
+</template>
