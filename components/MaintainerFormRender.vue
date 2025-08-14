2969ed7f2edbe8320b051c5d514c6b04f9ca9269
Dilip G <igoldlip@gmail.com>
2025-08-14 17:31:05 +0530

    use v-html to render html tags for rich formatting

--- components/MaintainerFormRender.vue
+++ components/MaintainerFormRender.vue
@@ -5,7 +5,7 @@ defineProps(["maintainer"]);
   <div class="flex flex-col p-8 gap-12">
     <template v-for="item in maintainer.form" :key="item.question">
       <div
-        class="flex flex-col gap-6 text-base font-normal"
+        class="flex flex-col gap-6 text-base font-normal sans-text"
         v-if="item.response"
       >
         <div class="bg-secondary-light dark:bg-secondary-dark p-2">
@@ -14,9 +14,7 @@ defineProps(["maintainer"]);
           </p>
         </div>
         <div>
-          <p class="text-secondary-light dark:text-secondary-dark">
-            {{ item.response }}
-          </p>
+          <p class="text-secondary-light dark:text-secondary-dark" v-html="item.response"></p>
         </div>
       </div>
     </template>
