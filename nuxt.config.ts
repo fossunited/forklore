412c66953daf6790a0d875a5a01146b15d80ef1a
Dilip <igoldlip@gmail.com>
2026-02-24 21:10:03 +0530

    init rss feed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ["/rss", "/"],
+      routes: ["/rss", "/","/planet/rss"],
     },
   },
   ogImage: {
